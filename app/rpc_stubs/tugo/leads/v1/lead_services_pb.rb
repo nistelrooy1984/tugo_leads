cb8fb9d619a28000fcd162515da704c23e978e48
quoctuan <quoctuan@DESKTOP-BRQSBKE.localdomain>
2021-07-15 16:46:17 +0700

    [TUGO-26] Tạo API get thông tin master Country, Province, District, Ward

--- app/rpc_stubs/tugo/leads/v1/lead_services_pb.rb
+++ app/rpc_stubs/tugo/leads/v1/lead_services_pb.rb
@@ -9,6 +9,7 @@ module Tugo
     module V1
       module LeadService
         class Service
+
           include ::GRPC::GenericService
 
           self.marshal_class_method = :encode
@@ -35,6 +36,10 @@ module Tugo
           # led_00005 Upsert Leads
           # --
           rpc :UpsertLeads, ::Tugo::Leads::V1::UpsertLeadsRequest, ::Tugo::Leads::V1::UpsertLeadsResponse
+          # --
+          # led_00006 Get All Master
+          # --
+          rpc :GetAllMaster, ::Google::Protobuf::Empty, ::Tugo::Leads::V1::AllMasterResponse
         end
 
         Stub = Service.rpc_stub_class
