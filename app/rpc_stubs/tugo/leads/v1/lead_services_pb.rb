ed1818131c0233291f69f7c18b3c2ce651cefaea
quoctuan <quoctuan@DESKTOP-BRQSBKE.localdomain>
2021-09-13 00:42:24 +0700

    [TUGO-60] Cteate API get masters

--- app/rpc_stubs/tugo/leads/v1/lead_services_pb.rb
+++ app/rpc_stubs/tugo/leads/v1/lead_services_pb.rb
@@ -36,6 +36,10 @@ module Tugo
           # led_00005 Upsert Leads
           # --
           rpc :UpsertLeads, ::Tugo::Leads::V1::UpsertLeadsRequest, ::Tugo::Leads::V1::UpsertLeadsResponse
+          # --
+          # led_00006 Search Leads
+          # --
+          rpc :SearchLeads, ::Tugo::Leads::V1::SearchRequest, ::Tugo::Leads::V1::LeadsResponse
         end
 
         Stub = Service.rpc_stub_class
